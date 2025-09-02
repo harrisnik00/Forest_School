6c9f4ef829196d428daeb72b84892745fc6b9daf
harrisnik00 <harrisnik000@gmail.com>
2025-09-02 18:17:16 +0300

    some changes

--- pages/admin.py
+++ pages/admin.py
@@ -4,25 +4,20 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Page, TeamMember, FAQ, AcademicCalendar
 
+
 @admin.register(Page)
 class PageAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "published", "updated_at")
-    prepopulated_fields = {"slug": ("title",)}
-
+    list_display= ("title", "slug")
+    search_fields = ("title","slug")
+    prepopulated_fields = {"slug":("title",)}
 
 @admin.register(TeamMember)
 class TeamMemberAdmin(admin.ModelAdmin):
-    list_display = ("name", "role", "order")
-    ordering = ("order",)
+    list_display= ("name", "role")
+    search_fields = ("name", "role")
 
 
 @admin.register(FAQ)
 class FAQAdmin(admin.ModelAdmin):
-    list_display = ("question", "order")
-    ordering = ("order",)
-
-
-@admin.register(AcademicCalendar)
-class AcademicCalendarAdmin(admin.ModelAdmin):
-    list_display = ("year", "published", "created_at")
-
+    list_display= ("question", )
+    search_fields = ("question", "answer")
