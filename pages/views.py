a49088ba757dc41c66c0e0af7b5ffdf6be0f18dc
harrisnik00 <harrisnik000@gmail.com>
2025-09-03 20:30:58 +0300

    fixed contact and pages

--- pages/views.py
+++ pages/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import redirect
-from django.core.mail import send_mail
-from django.conf import settings
-from .forms import ContactForm
-from django.contrib import messages
+from django.shortcuts import render
 
-def contact_submit(request):
-    if request.method == "POST":
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            send_mail(
-                subject=form.cleaned_data['subject'],
-                message=f"Name: {form.cleaned_data['name']}\nEmail: {form.cleaned_data['email']}\n\nMessage:\n{form.cleaned_data['message']}",
-                from_email=settings.DEFAULT_FROM_EMAIL,
-                recipient_list=['contact@example.com'],  # replace with your email
-                fail_silently=False,
-            )
-            messages.success(request, "Thank you! Your message has been sent.")
-    return redirect('core:index')
+def index(request):
+    return render(request, "core/index.html")
