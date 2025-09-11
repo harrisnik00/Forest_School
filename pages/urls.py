072c180d94939035306a76ac46c368acca7d1fd4
harrisnik00 <harrisnik000@gmail.com>
2025-09-11 09:34:38 +0300

    fixing more template errors

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,7 @@ app_name = "pages"
 
 urlpatterns = [
     path("", views.home , name="index"),
+    path("portfolio-details/", views.portfolio_details, name="portfolio_details"),
+    path("service-details/", views.services_details, name="services_details"),
 ]
+
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+def portfolio_details(request):
+    return render(request, "portfolio-details.html")
+
+def services_details(request):
+    return render(request, "service-details.html")
